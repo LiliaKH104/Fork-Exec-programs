9343a771905e4d890bef32a449e0aa47c1b2b210
Lilia Khachatryan <lilia.khachatryann@gmail.com>
2026-09-18 05:23:46 +0000

    updated

--- assignment0.c
+++ assignment0.c
@@ -1,18 +1,55 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-int main()
-{
-	printf("Before 1st fork\n");
-        fork();
-	printf("After 1st fork\n");
+#include <stdint.h>
+int main(){
 
-        fork();
-	printf("After 2nd fork\n");
+	pid_t pid;
+	printf("initial PID:%jd\n", (intmax_t)getpid());
+	pid= fork();
+	if(pid<0){
+		printf("Fork is not working");
+		return 1;
+	}
+	else if(pid==0){
+		printf("\n");
 
-        fork();
-	printf("After 3rd fork\n");
-
-
-        return 0;
+		printf("Child PID: %jd\n", (intmax_t)getpid());
+		printf("Parent PID:%jd\n",(intmax_t)getppid());
+		printf("\n");
+		
+		pid=fork();
+		if(pid<0){
+			printf("Second Fork is not working");
+			return 1;
+		}
+		else if (pid==0){
+			printf("Grandson PID:%jd\n",(intmax_t)getpid());
+			printf("Child PID:%jd\n",(intmax_t)getppid());
+		}
+		else{
+			printf("Child PID :%jd\n", (intmax_t)getpid());
+			sleep(2);
+		}
+			
+	}
+	else{
+		printf("Parent PID:%jd\n", (intmax_t)getpid());
+		sleep(2);
+		pid = fork();
+		if(pid<0){
+			printf("Third Fork is not working");
+			return 1;
+		}
+		else if(pid==0){
+			printf("new Child PID:%jd\n",(intmax_t)getpid());
+			printf("Parent PID: %jd\n",(intmax_t)getppid());
+		}
+		else{
+			printf("Parent PID:%jd\n",(intmax_t)getpid());
+			sleep(2);
+		}
+	}
+	return 0;
 }
+
